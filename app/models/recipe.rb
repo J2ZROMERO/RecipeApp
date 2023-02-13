ead0a5ea4dd0944bab5cfc43f78747c18edd0f31
Jhosep <jose-zromero@hotmail.com>
2023-02-13 15:33:36 -0600

    Add validates to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :recipe_foods, foreign_key: :recipe_id
+
+  validates :name length: { maximum: 100 }, presence: true
+  validates :public, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :foods, foreign_key: :user_id
   has_many :recipes, foreign_key: :user_id
+
+  validates :name, length: { maximum: 80 }, presence: true
 end
