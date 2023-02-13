ab40806cf30049b78f26ecbfeeffc370c0983f05
Jhosep <jose-zromero@hotmail.com>
2023-02-13 15:46:35 -0600

    Add test to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   has_many :recipe_foods, foreign_key: :recipe_id
 
-  validates :name length: { maximum: 100 }, presence: true
+  validates :name, length: { maximum: 100 }, presence: true
   validates :public, presence: true
 
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,14 +1,20 @@
+require 'database_cleaner'
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Recipe, type: :model do
   before(:each) do
     @recipe = Recipe.new
+    
   end
 
-  describe 'When the user model is tested' do
-    it 'validates the name of the user' do
-      @first_user.name = 'Jose'
-      expect(@first_user.valid?).to be true
+  describe 'When the recipes model is tested' do
+    it 'validates the recipe object' do
+      @recipe.name = 'Cookie'
+      @recipe.public = true
+      
+      expect(@recipe.name).to eq "Cookie"
     end
   end
+
 end
