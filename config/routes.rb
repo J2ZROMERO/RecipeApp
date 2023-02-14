62a4a7e5534c2daa9054b770afa751d70e0c687f
Jhosep <jose-zromero@hotmail.com>
2023-02-14 16:25:11 -0600

    Add letter_opener gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'users/confirmations', passwords: 'users/passwords',registrations: 'users/registrations', sessions: 'users/sessions' }
+
+  devise_scope :user do
+    root to: "users/sessions#new"
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    get '/users/password', to: 'users/passwords#create'
+  end
+  
+  
   resources :recipes
   resources :recipe_foods
   resources :foods
