fc1ee6f6ff8ddf0696e6272244036b9c195fefbc
Jhosep <jose-zromero@hotmail.com>
2023-02-14 17:04:17 -0600

    Add login and register user

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,9 +2,9 @@
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/confirmation
   # def create
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to user_session_path
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name ,:email, :password, :password_confirmation)
+  end
+
 
   # GET /resource/edit
   # def edit
