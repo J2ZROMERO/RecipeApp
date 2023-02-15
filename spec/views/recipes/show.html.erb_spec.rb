6036a328a05938ffb6c221c25a9125044dd8b5c0
Jhosep <jose-zromero@hotmail.com>
2023-02-15 16:23:17 -0600

    Apply test to view

--- spec/views/recipes/show.html.erb_spec.rb
+++ spec/views/recipes/show.html.erb_spec.rb
@@ -3,7 +3,8 @@ require 'database_cleaner'
 
 RSpec.describe 'recipes/show', type: :view do
   before(:each) do
-    @user1 = User.create(name: "jose1test", email: 'user1@hotmail.com', password: 123456, password_confirmation: 123456)
+    @user1 = User.create(name: 'jose1test', email: 'user1@hotmail.com', password: 123_456,
+                         password_confirmation: 123_456)
     @recipe = assign(:recipe, Recipe.create!(
                                 name: 'Name',
                                 preparation_time: 'Preparation Time',
@@ -16,9 +17,7 @@ RSpec.describe 'recipes/show', type: :view do
 
   it 'renders attributes in <p>' do
     render
-    expect(rendered).to match(/Name/)   
+    expect(rendered).to match(/Name/)
     expect(rendered).to match(/MyText/)
-    
-    
   end
 end
