685c9e95caef3efe20b863a3f8c3aeef82ccca5d
Oklukeok <lucasabermudez@gmail.com>
2023-02-16 19:30:29 -0600

    Update

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,8 @@ class FoodsController < ApplicationController
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all.order(name: :desc)
+    @foods = Food.order(params[:sort])
+   
   end
 
   # GET /foods/1 or /foods/1.json
--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,10 @@
 class PublicRecipesController < ActionController::Base
     def index
       @public_recipes = Recipe.all
+      @recipes_foods = RecipeFood.all
+      @foodie = Food.all
+      
     end
   
+    
 end
