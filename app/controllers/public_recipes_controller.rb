d5d4bde4b0c4b2112450ea9de4ca3d96289b9d04
Oklukeok <lucasabermudez@gmail.com>
2023-02-17 12:10:11 -0600

    Added Items Sum and Total cost for each recipe

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,7 +2,7 @@ class PublicRecipesController < ActionController::Base
     def index
       @public_recipes = Recipe.all
       @recipes_foods = RecipeFood.all
-      @foodie = Food.all
+      @foodie = RecipeFood.all
       
     end
   
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,6 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :foods, foreign_key: :food_id
-  belongs_to :recipes, foreign_key: :recipe_id
+  belongs_to :food, foreign_key: :food_id
+  belongs_to :recipe, foreign_key: :recipe_id
 
   validates :quantity, presence: true
 end
