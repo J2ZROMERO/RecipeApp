3c85ca6061ed9bb88b92029c2f5e2b91757debdf
Jhosep <jose-zromero@hotmail.com>
2023-02-17 12:14:56 -0600

    Add current user for queries

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
+  load_and_authorize_resource
 
   # GET /recipes or /recipes.json
   def index
